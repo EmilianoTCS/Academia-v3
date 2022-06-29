b8b00d2809607a417aceb5addd84689984f98f6a
EmilianoTCS <emi.vega84@gmail.com>
2022-06-29 18:08:37 -0300

    29/06 18:10

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { useState } from "react";
 import HomePage from "./components/homePage";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 import Login from "./components/login";
 import ListadoCursos from "./components/TASKS/ListadoCursos";
 import ListadoRamos from "./components/TASKS/ListadoRamos";
@@ -10,25 +10,29 @@ import ListadoRelator from "./components/TASKS/ListadoRelator";
 import ReporteGeneral from "./components/TASKS/ReporteGeneral";
 import ListadoColaboradores from "./components/TASKS/ListadoColaboradores";
 function App() {
+
   const [conectado, setConectado] = useState(false);
 
   const acceder = (estado) => {
     setConectado(estado);
   }
-
+  
+  if(conectado){
   return (
     <Router>
-
-
+      <Redirect to="/home" />
       <Route path="/home" component={HomePage}></Route>
       <Route exact path="/" component={HomePage}></Route>
-      <Route exact path={"/login"} component={Login}></Route>
+      <Route path={"/login"} component={Login}></Route>
       <Route path="/Cursos" component={ListadoCursos}></Route>
       <Route path="/Ramos" component={ListadoRamos}></Route>
       <Route path="/Relator" component={ListadoRelator}></Route>
       <Route path="/reporteGeneral" component={ReporteGeneral}></Route>
       <Route path="/Colaboradores" component={ListadoColaboradores}></Route>
     </Router>
+  );
+  } return(
+    <Login acceder={acceder}></Login>
   );
 }
 
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,16 +1,32 @@
 import React, {Component} from "react";
-
+import { Link } from "react-router-dom";
 import Header from "./templates/header";
-
+import './css/Modulos.css'
 class HomePage extends Component {
     render() { 
         return (
         <div>
              <Header></Header>
+            <div id="container_mods">
+                <div className="cartas">
+                    <div className="cuerpo-carta">
+                        <h4 className="titulo-carta">Navegador</h4>
+                        <p className="texto-carta"><Link style={{ textDecoration: 'none', color: 'white' }}to={"/reporteGeneral"}>Reporte General</Link></p>
+                        <p className="texto-carta"><Link style={{ textDecoration: 'none', color: 'white' }}to={"/Cursos"}>Cursos</Link></p>
+                        <p className="texto-carta"><Link style={{ textDecoration: 'none', color: 'white' }}to={"/Ramos"}>Ramos</Link></p>
+                        <p className="texto-carta"><Link style={{ textDecoration: 'none', color: 'white' }}to={"/Relator"}>Relator</Link></p>
+                    </div>
+                </div>
+                <div className="cartas">
+                    <div className="cuerpo-carta">
+                        <h4 className="titulo-carta">Temarios</h4>
+                        <p className="texto-carta"><a style={{ textDecoration: 'none', color: 'white' }}href="https://github.com/EmilianoTCS/Academia-v3/raw/master/Temarios/Temario%20Capacitaci%C3%B3n%20Jira%202.1%20(1)%20(1).pdf">JIRA</a></p>
+                        <p className="texto-carta"><a style={{ textDecoration: 'none', color: 'white' }}href="https://github.com/EmilianoTCS/Academia-v3/raw/master/Temarios/Temario%20Capacitaci%C3%B3n%20GIT.pdf">GIT</a></p>
+                    </div>
+                </div>
 
 
-
-
+            </div>
         </div> 
           );
     }
